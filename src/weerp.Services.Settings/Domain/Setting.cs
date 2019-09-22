4a18ecd0c6964a00d103bdcf1368d8a23e2bf18a
Ambert Jean-Christophe <jc.ambert@free.fr>
2019-09-22 19:41:44 +0200

    First Commit

---
+++ src/weerp.Services.Settings/Domain/Setting.cs
@@ -0,0 +1,20 @@
+﻿using MicroS_Common.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace weerp.Services.Settings.Domain
+{
+    public class Setting : BaseEntity
+    {
+        public Setting(Guid id) : base(id)
+        {
+
+        }
+
+        public string Numero { get; private set; }
+
+        public string Nom { get; private set; }
+    }
+}
